f1026ced88ef1625db7cdb4dffe13d45ebe1357f
konstrictor <jonkur96@gmail.com>
2020-10-09 11:51:15 +0200

    model version initial

--- API/admin.py
+++ API/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Compagnie)
+admin.site.register(Avion)
+admin.site.register(Aeroport)
+admin.site.register(Vol)
+admin.site.register(Reservation)
--- API/models.py
+++ API/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Compagnie(models.Model):
+	nom = models.CharField(max_length=32)
+
+	def __str__(self):
+		return f"{self.nom}"
+
+class Avion(models.Model):
+	compagnie = models.ForeignKey("Compagnie", on_delete=models.CASCADE)
+	model = models.CharField(max_length=16)
+
+	def __str__(self):
+		return f"{self.model} - {self.compagnie}"
+
+class Aeroport(models.Model):
+	nom = models.CharField(max_length=64)
+	ville = models.CharField(max_length=32)
+
+	def __str__(self):
+		return f"{self.nom} - {self.ville}"
+
+class Vol(models.Model):
+	date = models.DateTimeField(default=timezone.now)
+	avion = models.ForeignKey("Avion", on_delete=models.PROTECT)
+	source = models.ForeignKey("Aeroport", related_name='source', on_delete=models.PROTECT)
+	destination = models.ForeignKey("Aeroport", related_name='destination', on_delete=models.PROTECT)
+
+	def __str__(self):
+		return f"{self.source} -> {self.destination} à {self.date}"
+
+class Reservation(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	vol = models.ForeignKey(Vol, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return f"{self.user.firstname} {self.vol}"
