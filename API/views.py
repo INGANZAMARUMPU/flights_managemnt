963bf226c1ca699d46ebd8f539b038410ff27618
konstrictor <jonkur96@gmail.com>
2020-10-20 18:23:32 +0200

    permit token authentication on Vol, User, Reservation views

--- API/views.py
+++ API/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.authentication import SessionAuthentication
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -9,7 +9,7 @@ from .serializers import *
 from .models import *
 
 class UserViewset(viewsets.ModelViewSet):
-	authentication_classes = [SessionAuthentication,]
+	authentication_classes = [SessionAuthentication, TokenAuthentication]
 	permission_classes = [IsAuthenticatedOrReadOnly, ]
 	serializer_class = UserSerializer
 	queryset = User.objects.all()
@@ -33,13 +33,13 @@ class AeroportViewset(viewsets.ModelViewSet):
 	queryset = Aeroport.objects.all()
 
 class VolViewset(viewsets.ModelViewSet):
-	authentication_classes = [SessionAuthentication,]
+	authentication_classes = [SessionAuthentication, TokenAuthentication]
 	permission_classes = [IsAuthenticatedOrReadOnly, ]
 	serializer_class = VolSerializer
 	queryset = Vol.objects.all()
 
 class ReservationViewset(viewsets.ModelViewSet):
-	authentication_classes = [SessionAuthentication,]
+	authentication_classes = [SessionAuthentication, TokenAuthentication]
 	permission_classes = [IsAuthenticatedOrReadOnly, ]
 	serializer_class = ReservationSerializer
 	queryset = Reservation.objects.all()
